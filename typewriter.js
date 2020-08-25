f0d6da471e7b4a68cb6a4b7244adbff51671ac40
Syrym Zhauken <syrym.zhauken@gmail.com>
2020-08-25 12:10:26 -0700

    stdout.write added instead of console.log

--- typewriter.js
+++ typewriter.js
@@ -2,7 +2,7 @@ const sentence = "hello there from lighthouse labs";
 let count = 0;
 for (const char of sentence) {
   setTimeout(() => {
-    console.log(`${char} \n`);
+    process.stdout.write(`${char}`);
   }, count);
   count += 50;
 }
